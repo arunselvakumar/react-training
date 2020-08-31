ee6c6c431b8c216d78189c3ea88bf7c787db1a5f
Arun Selva Kumar <b.arunselvakumar@gmail.com>
2020-08-31 07:54:49 +0530

    Added Rules + converted App.js to JSX

--- src/App.js
+++ src/App.js
@@ -3,6 +3,31 @@ import { render } from 'react-dom';
 import Pet from './Pet';
 
 const App = () => {
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet
+        id="something-important"
+        name="Luna 1"
+        animal="Dog 1"
+        breed="Havanes 1"
+      />
+      <Pet
+        id="something-important"
+        name="Luna 2"
+        animal="Dog 2"
+        breed="Havanes 2"
+      />
+      <Pet
+        id="something-important"
+        name="Luna 3"
+        animal="Dog 3"
+        breed="Havanes 3"
+      />
+    </div>
+  );
+
+  /*
   return React.createElement('div', {}, [
     React.createElement('h1', {}, 'Adopt Me!'),
     React.createElement(Pet, {
@@ -21,6 +46,8 @@ const App = () => {
       breed: 'Havanese 3',
     }),
   ]);
+
+  */
 };
 
-render(React.createElement(App), document.getElementById('root'));
+render(<App />, document.getElementById('root'));
