38294078381844b6d5dc1fc88212cab27ea33402
Arun Selva Kumar <b.arunselvakumar@gmail.com>
2020-09-02 08:40:54 +0530

    Added Mappers in JSX

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
+import { ANIMALS } from '@frontendmasters/pet';
 
 const SearchParams = () => {
   const [location, setLocation] = useState('Seattle, WA');
+  const [animal, setAnimal] = useState('Dog');
 
   return (
     <div className="search-params">
@@ -14,6 +16,25 @@ const SearchParams = () => {
             placeholder="Location"
             onChange={(e) => setLocation(e.target.value)}
           />
+        </label>
+
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            type="text"
+            value={animal}
+            placeholder="Animal"
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option value="">All</option>
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
