941b2103bf6cb77e20105fc1ce6fa8ab943c6203
Arun Selva Kumar <b.arunselvakumar@gmail.com>
2020-09-05 11:31:33 +0530

    Created UseDropDown Hook

---
+++ src/hooks/useDropDown.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+const useDropDown = (label, defaultState, options) => {
+  const [state, setState] = useState(defaultState);
+  const id = `use-dropdown-${label.replace(' ', '').toLowerCase()}`;
+  const DropDown = () => (
+    <label htmlFor={id}>
+      {label}
+      <select
+        id={id}
+        type="text"
+        value={state}
+        onChange={(e) => setState(e.target.value)}
+        onBlur={(e) => setState(e.target.value)}
+        disabled={!options.length}
+      >
+        <option value="">All</option>
+        {options.map((item) => (
+          <option key={item} value={item}>
+            {item}
+          </option>
+        ))}
+      </select>
+    </label>
+  );
+
+  return [state, DropDown, setState];
+};
+
+export default useDropDown;
